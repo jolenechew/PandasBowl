af01aa576fd485483816b80f86f237fbf0c0e343
JieEnT <jieen1000@gmail.com>
2022-11-12 11:33:40 +0800

    feat: added telegram app

--- pandasbowl/src/views/FoodCardView.vue
+++ pandasbowl/src/views/FoodCardView.vue
@@ -7,6 +7,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faClock, faUtensils } from "@fortawesome/free-solid-svg-icons";
 import {useRoute} from "vue-router";
+import TelegramButton from "vue-share-buttons/src/components/TelegramButton";
 library.add(faClock, faUtensils);
 export default {
   el: "#app",
@@ -159,6 +160,7 @@ export default {
     "font-awesome-icon": FontAwesomeIcon,
     GoogleMap,
     Marker,
+    TelegramButton
   },
   methods: {
     // update all the calories, fat, protein, carbs properties
@@ -260,29 +262,47 @@ export default {
 
 <template>
   <div id="app" class="h-screen">
-    <div class="text-center">
+    <div class="text-center mx-2">
       <div class="header-title mt-20 mb-4">{{ foodName }}</div>
       <i class="words">{{info}}</i>
       <br>
       <br>
       <div class="mx-auto">
+        <div class="inline-block mr-8 text-xl subtitle">
+          <font-awesome-icon icon="fa-solid fa-clock" />
+          {{ time }} min
+        </div>
         <div class="inline-block ml-8 text-xl subtitle">
           <font-awesome-icon icon="fa-solid fa-utensils" />
           <div class="ml-4 inline-block">Servings: {{ servings }}</div>
         </div>
+        <br>
+        <br>
+        <div>
+          <div class="mx-auto md:mr-16 lg:mr-32 md:float-right">
+            <button class="btn btn-accent mt-1" @click="unlikeItem" v-if="isLiked">Unlike</button>
+            <button class="btn btn-accent mt-1" @click="likeItem" v-else> <svg style="color: red" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart-fill" viewBox="0 0 16 16"> <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z" fill="red"></path> </svg><span class="capitalize font-sans text-base font-semibold">&nbsp; Like</span></button>
+            <telegram-button class="w-28"
+              url="http://localhost:3001/foodCardView"  
+              description="Look at what I found! Let's try this recipe together!" 
+              customIcon="../assets/download.svg" btnText="Share"
+            />
+          </div>
+        
+        </div>
       </div>
-      <div class="btn-group float-right mr-32">
+      <!-- <div class="btn-group float-right mr-32">
         <button class="btn btn-accent" @click="unlikeItem" v-if="isLiked">Unlike</button>
         <button class="btn btn-accent" @click="likeItem" v-else>Like</button>
         <button class="btn btn-primary">Share</button>
-      </div>
+      </div> -->
     </div>
 
-    <div class="flex flex-wrap mt-16 mx-32">
-      <div class="w-full md:w-2/5 grey h-full">
+    <div class="grid grid-rows-6 grid-cols-6 mt-16 sm:mx-2 md:mx-16 lg:mx-32">
+      <div class="col-span-6 md:col-span-2">
         <img :src="foodImage" alt="img" style="min-height:700px;"/>
       </div>
-      <div class="w-full md:w-3/5 bg-color p-8">
+      <div class="col-span-6 md:col-span-4 bg-color p-8 order-first md:order-last">
         <div>
           <span class="font-bold text-xl">Diet type: </span>
           <span class="words" v-for="(dietEntry, index) in diet" :key="index">
@@ -349,5 +369,5 @@ export default {
         </div>
       </div>
     </div>
-  </div>
+</div>
 </template>
